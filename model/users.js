91ab5fd068ec2babc1e196de8cef5847a5a8d634
noaleshem <noaleshem03@gmail.com>
2020-03-02 15:12:26 +0200

    fix age and name

--- model/users.js
+++ model/users.js
@@ -1,18 +1,25 @@
 const csvFilePath='data.csv';
 const csv=require('csvtojson');
+const trie = require('trie-prefix-tree');
+const names_trie = trie([]);
 const id_to_user = {};
 const name_to_user = {};
 const country_to_user = {};
 const age_to_user = {};
+const dob_to_user = {};
 csv().fromFile(csvFilePath).then((jsonObj)=>{
     jsonObj.map(function(item){
         //map users by id
         id_to_user[item.Id] = item;
 
-        //map users by name
+        //map users by name into maps and prefix tree
         let name_arr = item.Name.split(" ");
-        let first = name_arr[0].substring(0, 3).toUpperCase();
-        let last = name_arr[1].substring(0, 3).toUpperCase();
+        let first = name_arr[0].toLowerCase();
+        let last = name_arr[1].toLowerCase();
+        //save the names in prefix tree
+        names_trie.addWord(first);
+        names_trie.addWord(last);
+        //save the users by names
         if (name_to_user[first] === undefined)
             name_to_user[first] = [];
         name_to_user[first] = name_to_user[first].concat([item]);
@@ -25,24 +32,12 @@ csv().fromFile(csvFilePath).then((jsonObj)=>{
             country_to_user[item.Country] = [];
         country_to_user[item.Country] = country_to_user[item.Country].concat([item]);  
 
-        //map users by age
-        let age = calc_age(item.DOB);
-        if (age_to_user[age] === undefined)
-            age_to_user[age] = [];
-        age_to_user[age] = age_to_user[age].concat([item]);
+        //map users by dob
+        if (dob_to_user[item.DOB] === undefined)
+            dob_to_user[item.DOB] = [];
+        dob_to_user[item.DOB] = dob_to_user[item.DOB].concat([item]);
     });
-})
- 
- function calc_age(dob_str){
-     let dob_arr = dob_str.split("/");
-     let day = Number(dob_arr[0]);
-     let month = Number(dob_arr[1]) - 1;
-     let year = Number(dob_arr[2]);
-     let dob = new Date(year, month, day);
-     let dates_dif = Date.now() - dob.getTime();
-     let age_date = new Date(dates_dif);
-     return Math.abs(age_date.getUTCFullYear() - 1970);
- }
+}) 
 
 module.exports = {
     getUserById: async function(id){
@@ -52,7 +47,27 @@ module.exports = {
 
     getUsersByAge: async function(age) {
         console.log(`getUsersByAge called with age: ${age}`);
-        return age_to_user[age];
+        //low_range is the lowest birth date for user to be *age* years old
+        let low_range = new Date();
+        low_range.setFullYear(low_range.getFullYear() - age - 1);
+        low_range.setDate(low_range.getDate() + 1);
+
+        //high_range is the highest birth date for user to be *age* years old
+        let high_range = new Date();
+        high_range.setFullYear(high_range.getFullYear() - age);
+        let res = [];
+
+        //iterate all birth dates that user can have to be *age* years old 
+        while (low_range <= high_range){
+            let year = low_range.getFullYear().toString();
+            let month = (low_range.getMonth() + 1).toString();
+            let day = low_range.getDate().toString();
+            let dob = day + "/" + month + "/" + year;
+            //concat all users with this DOB
+            res = res.concat(dob_to_user[dob]);
+            low_range.setDate(low_range.getDate() + 1);
+        }
+        return res;
     },
 
     getUsersByCountry: async function(country) {
@@ -62,21 +77,33 @@ module.exports = {
 
     getUsersByName: async function(name) {
         console.log(`searchUsersByName called with name: ${name}`);
-        name = name.substring(0, 3).toUpperCase();
-        return name_to_user[name];
+        name = name.toLowerCase();
+        //names_arr is array of all names that match to the query (full/partial match)
+        let names_arr = names_trie.getPrefix(name);
+        let res = [];
+        for (let i=0; i<names_arr.length; i++){
+            //concat all users with that name
+            res = res.concat(name_to_user[names_arr[i]]);
+        }
+        return res;
     },
 
     deleteUser: async function(id) {
         console.log(`deleteUser called with id: ${id}`);
         let country = id_to_user[id].Country;
-        let age = calc_age(id_to_user[id].DOB);
+        let dob = id_to_user[id].DOB;
         let name_arr = id_to_user[id].Name.split(" ");
-        let first = name_arr[0].substring(0, 3).toUpperCase();
-        let last = name_arr[1].substring(0, 3).toUpperCase();
+        let first = name_arr[0].toLowerCase();
+        let last = name_arr[1].toLowerCase();
+
         country_to_user[country] = country_to_user[country].filter(x => x.Id !== id);
-        age_to_user[age] = age_to_user[age].filter(x => x.Id !== id);
+        dob_to_user[dob] = dob_to_user[dob].filter(x => x.Id !== id);
         name_to_user[first] = name_to_user[first].filter(x => x.Id !== id);
         name_to_user[last] = name_to_user[last].filter(x => x.Id !== id);
+        if (name_to_user[first] === [])
+            names_trie.removeWord(first);
+        if (name_to_user[last] === [])
+            names_trie.removeWord(last);
         id_to_user[id] = {};
         return;
     }
